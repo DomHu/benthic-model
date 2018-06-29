ed3d20e7d29ce9a4a9fadb7e09d150f18698ea4b
domhu <dominik.huelse@gmx.de>
2018-06-29 16:29:23 -0700

    added Copernicus typesetting files Matlab test case for P-cycle

--- Two_fractions/benthic_test.m
+++ Two_fractions/benthic_test.m
@@ -26,13 +26,13 @@ classdef benthic_test
             bsd = benthic_main();
             %bottom water concentrations
             swi.T = 8.0;                                        % temperature (degree C)
-            swi.C01_nonbio= 1.01*1e-2/12*bsd.rho_sed;            % TOC concentration at SWI (wt%) -> (mol/cm^3 bulk phase)
-            swi.C02_nonbio= 1.01*1e-2/12*bsd.rho_sed;            % TOC concentration at SWI (wt%) -> (mol/cm^3 bulk phase)
+            swi.C01_nonbio= 0.2*1e-2/12*bsd.rho_sed;            % TOC concentration at SWI (wt%) -> (mol/cm^3 bulk phase)
+            swi.C02_nonbio= 0.2*1e-2/12*bsd.rho_sed;            % TOC concentration at SWI (wt%) -> (mol/cm^3 bulk phase)
             swi.Fnonbio1 = swi.C01_nonbio*(1-bsd.por)*bsd.w;    % calculate flux [mol/(cm2 yr)] according non-bioturbated flux
             swi.Fnonbio2 = swi.C02_nonbio*(1-bsd.por)*bsd.w;    % calculate flux [mol/(cm2 yr)] according non-bioturbated flux
             swi.C01 = swi.C01_nonbio;                           % resulting bioturbated SWI-concentration, to be calculated in benthic_zTOC.m
             swi.C02 = swi.C02_nonbio;                           % resulting bioturbated SWI-concentration, to be calculated in benthic_zTOC.m
-            swi.O20=0.0E-009;                                 % O2  concentration at SWI (mol/cm^3)
+            swi.O20=30.0E-009;                                 % O2  concentration at SWI (mol/cm^3)
             swi.NO30=40.0e-9;                                   % NO3 concentration at SWI (mol/cm^3)
             swi.Nitrogen=true;                                  % calculate N (true/false)
             swi.NH40=10.0e-9;                                 	% NH4 concentration at SWI (mol/cm^3)
@@ -88,7 +88,7 @@ classdef benthic_test
                     res=benthic_test.test_benthic(1,swi);
                     SWI_PO4(i,j)=res.flxswi_P;
                     O20(j)=(j-1)*3.0E-009;
-                if(i==1 && j==45)
+                if(i==10 && j>=10)
                     benthic_test.plot_column(res, false, swi, '0E-9_O2_i1_j45')
                 end
                 end                
@@ -96,14 +96,14 @@ classdef benthic_test
             
             figure;
             hold on
-            [C,h] = contourf(Corg,O20,SWI_PO4);
+            [C,h] = contourf(O20,Corg,SWI_PO4);
             clabel(C,h,'FontSize',16);
             colorbar();
             box on
             hold off
-            xlabel ({'Corg'}); %;'(\mumol cm^{-2}yr^{-1})'})
-            ylabel('Ocean O_2')    
-            print('-depsc', 'PO4_SWI-flux_SA_2606_4PO4_00100001_lowtohighCorg_150O2_3000m');
+            xlabel('Ocean O_2')    
+            ylabel ({'Corg'}); %;'(\mumol cm^{-2}yr^{-1})'})
+            print('-depsc', 'PO4_SWI-flux_SA_2706_4PO4_00100001_lowtohighCorg_150O2_3000m');
         
         end
         
